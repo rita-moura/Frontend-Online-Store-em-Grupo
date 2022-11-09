af3e33674194f23f81bc63addc454a799f961261
Ikarosv <ikarovieiraa@gmail.com>
2022-11-09 17:44:23 -0300

    feat: productListing implementado
    A page ProductListing foi implementada, agora ela tem um stado chamado: productList, se esse estado tiver vazio ele exibe uma mensagem pedindo para o usuário digitar no input, se tiver algo, ele faz um map no productList exibindo uma div com a mengagem: generic message, mensagem esta que poderá ser trocada de acordo com os requisitos.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
 import ProductListing from './pages/ProductListing';
 
 function App() {
--- src/pages/ProductListing.jsx
+++ src/pages/ProductListing.jsx
@@ -1,9 +1,31 @@
 import React, { Component } from 'react';
 
 export default class ProductListing extends Component {
+  state = {
+    productList: [],
+  };
+
   render() {
+    const { productList } = this.state;
+
     return (
-      <div>ProductListing</div>
+      <section>
+        <input type="text" />
+        {
+          !productList.legth
+            ? (
+              <h2
+                data-testid="home-initial-message"
+              >
+                Digite algum termo de pesquisa ou escolha uma categoria.
+              </h2>
+            ) : (
+              productList.map((product, index) => (
+                <div key={ index }>Generic message</div>
+              ))
+            )
+        }
+      </section>
     );
   }
 }
