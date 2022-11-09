67be164513a87e6bdce688178444fb351ceab3d8
Ikarosv <ikarovieiraa@gmail.com>
2022-11-09 19:00:33 -0300

    fix: mudando o svg e a exportação
    Mudei o export de ExportImages, antes estava como default, agora só tá o export. Consertei o svg Back, antes estava com a palavra voltar.

--- src/assets/ExportImages.js
+++ src/assets/ExportImages.js
@@ -1,7 +1,7 @@
 import { ReactComponent as BackSvg } from './images/Back.svg';
 import { ReactComponent as CartSvg } from './images/Cart.svg';
 
-export default {
+export {
   BackSvg,
   CartSvg,
 };
