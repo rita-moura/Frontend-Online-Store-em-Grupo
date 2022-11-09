b8b349c90214f37dca25c8c5da4e66710250e403
Ikarosv <ikarovieiraa@gmail.com>
2022-11-09 18:29:09 -0300

    feat: adiciona state e mensangem
    Adicioando o state CartState (State no final para diferenciar do componente) ao componente cart. Coloquei também a mensagem dizendo que o carrinho está limpo, e o map foi só para facilitar futuras mudanças

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,25 @@
 import React, { Component } from 'react';
 
 export default class Cart extends Component {
+  state = {
+    CartState: [],
+  };
+
   render() {
+    const { CartState } = this.state;
+
     return (
-      <div>Cart</div>
+      <main>
+        {
+          !CartState.length ? (
+            <h2 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h2>
+          ) : (
+            CartState.map((cartProduct, index) => (
+              <div key={ index }>Generic Message</div>
+            ))
+          )
+        }
+      </main>
     );
   }
 }
